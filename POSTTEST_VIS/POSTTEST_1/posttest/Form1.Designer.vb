e3c6ca2db9d131778e395d2c464009fdbfc224ca
kelvin alviansyah <104289852+kelvinaavas@users.noreply.github.com>
2026-03-01 22:53:37 +0800

    POSTTEST_VIS

---
+++ POSTTEST_VIS/POSTTEST_1/posttest/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtIP = New TextBox()
+        btnTambah = New Button()
+        Label2 = New Label()
+        txtIPK = New TextBox()
+        lblPredikat = New Label()
+        btnReset = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(239, 288)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(167, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "IP SEMESTER"
+        ' 
+        ' txtIP
+        ' 
+        txtIP.Location = New Point(431, 288)
+        txtIP.Name = "txtIP"
+        txtIP.Size = New Size(200, 39)
+        txtIP.TabIndex = 1
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.BackColor = Color.FromArgb(CByte(192), CByte(255), CByte(192))
+        btnTambah.Cursor = Cursors.IBeam
+        btnTambah.Font = New Font("Verdana", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnTambah.ForeColor = Color.Green
+        btnTambah.Location = New Point(240, 352)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(150, 46)
+        btnTambah.TabIndex = 2
+        btnTambah.Text = "TAMBAH"
+        btnTambah.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(239, 436)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(189, 32)
+        Label2.TabIndex = 3
+        Label2.Text = "IP KUMULATIF"
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.Enabled = False
+        txtIPK.Location = New Point(431, 436)
+        txtIPK.Name = "txtIPK"
+        txtIPK.Size = New Size(200, 39)
+        txtIPK.TabIndex = 4
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(239, 516)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(0, 32)
+        lblPredikat.TabIndex = 5
+        ' 
+        ' btnReset
+        ' 
+        btnReset.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btnReset.FlatStyle = FlatStyle.Popup
+        btnReset.Font = New Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnReset.ForeColor = Color.FromArgb(CByte(192), CByte(0), CByte(0))
+        btnReset.Location = New Point(396, 352)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(150, 46)
+        btnReset.TabIndex = 6
+        btnReset.Text = "RESET"
+        btnReset.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(1612, 996)
+        Controls.Add(btnReset)
+        Controls.Add(lblPredikat)
+        Controls.Add(txtIPK)
+        Controls.Add(Label2)
+        Controls.Add(btnTambah)
+        Controls.Add(txtIP)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "KALKULATOR IP"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents btnReset As Button
+
+End Class
---
+++ POSTTEST_VIS/POSTTEST_1/posttest/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+        Dim ipSemester As Double
+        If Double.TryParse(txtIP.Text, ipSemester) Then
+            totalIP += ipSemester
+            jumlahSemester += 1
+
+            Dim ipk As Double = totalIP / jumlahSemester
+            txtIPK.Text = ipk.ToString("0.00")
+
+            If ipk >= 3.01 Then
+                lblPredikat.Text = "Sangat Memuaskan"
+            ElseIf ipk >= 2.76 Then
+                lblPredikat.Text = "Memuaskan"
+            ElseIf ipk >= 2.0 Then
+                lblPredikat.Text = "Cukup"
+            End If
+
+            txtIP.Text = ""
+            txtIP.Focus()
+        End If
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        totalIP = 0
+        jumlahSemester = 0
+        txtIP.Text = ""
+        txtIPK.Text = ""
+        lblPredikat.Text = ""
+    End Sub
+
+
+End Class
